9f3fdecd7e5b8fe057a95a5aca025c4bda6c8ad0
ElyeserGabrian <gabriantec@gmail.com>
2025-10-29 23:14:19 -0300

    Ajuste na documentação e criação do .env

--- controle-gastos/src/main/java/com/projeto_backend/controle_gastos/ControleGastosApplication.java
+++ controle-gastos/src/main/java/com/projeto_backend/controle_gastos/ControleGastosApplication.java
@@ -1,5 +1,6 @@
 package com.projeto_backend.controle_gastos;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ControleGastosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ControleGastosApplication.class, args);
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        SpringApplication.run(ControleGastosApplication.class, args);
 	}
 
 }
